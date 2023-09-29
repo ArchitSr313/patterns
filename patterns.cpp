f6a4980971a599ea73c958b86e89b79d5ad690dc
Archit Srivastava <96858242+ArchitSr313@users.noreply.github.com>
2023-09-29 10:15:01 +0530

    Add files via upload

---
+++ patterns.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+using namespace std;
+int main(){
+    for (int i = 1; i < 6; i++)
+    {
+        for (int j = 1; j < 6; j++)
+        {
+            cout<<"*"<<" ";
+
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    cout<<endl;
+    for (int i = 1; i < 5; i++)
+    {
+        for (int j = 1; j < 6; j++)
+        {
+            cout<<"10"<<" ";
+
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    cout<<endl;
+    for (int i = 1; i < 6; i++)
+    {
+        for (int j = 1; j < 6; j++)
+        {
+            cout<<i<<" ";
+
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    cout<<endl;
+    for (int i = 1; i < 6; i++)
+    {
+        for (int j = 1; j < 6; j++)
+        {
+            cout<<j<<" ";
+
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    cout<<endl;
+    for (int i = 1; i < 6; i++)
+    {
+        for (int j = 5; j > 0; j--)
+        {
+            cout<<j<<" ";
+
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    cout<<endl;
+    for (int i = 1; i < 6; i++)
+    {
+        for (int j = 1; j < 6; j++)
+        {
+            cout<<j*j<<" ";
+
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    cout<<endl;
+    for (char i = 'a'; i < 'f'; i++)
+    {
+        for (char j = 'a'; j < 'f'; j++)
+        {
+            cout<<i<<" ";
+
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    cout<<endl;
+    for (char i = 'a'; i < 'f'; i++)
+    {
+        for (char j = 'a'; j < 'f'; j++)
+        {
+            cout<<j<<" ";
+
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    cout<<endl;
+    int count=1;
+    for (int i = 1; i < 6; i++)
+    {
+        
+        for (int j = 1; j < 6; j++)
+        {
+            cout<<count<<" ";
+            count++;
+
+        }
+        cout<<endl;
+    }
+}
